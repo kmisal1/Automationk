a77629b55e0f148d76647767ebc916df9096ef54
kmisal1 <kmisal95@gmail.com>
2023-05-19 23:11:37 +0530

    first commit is here

---
+++ src/breakContinue.java
@@ -0,0 +1,38 @@
+
+public class breakContinue {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		/*int x =0;
+		while (true)
+		{
+			if (x%3==0 && x>120)
+			{
+				System.out.println("the no divisible is"  );
+				break;
+			}
+			
+			
+			else
+		{
+			System.out.println(x);
+		}
+			x++;*/
+		
+	
+		int number=0 ;
+		while(true) {
+			if(number%3==0 && number>150) {
+				break;
+			}
+			else{
+				System.out.println(number);
+				
+			}
+			number++;
+	}
+	}
+}
+	
+
